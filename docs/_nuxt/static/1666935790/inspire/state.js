5dd4806818f1f426138d6526a5aaf0baef084b8f
roudai <me@roudai.net>
2022-10-28 14:48:31 +0900

    first commit

---
+++ docs/_nuxt/static/1666935790/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkomachan\u002F_nuxt\u002Fstatic\u002F1666935790",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fkomachan\u002F",assetsPath:"\u002Fkomachan\u002F_nuxt\u002F",cdnURL:a}}}}(null));
