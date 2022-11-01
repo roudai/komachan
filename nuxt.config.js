04f376330b86d9c35c51ae9eb62764adcdc1bd6f
roudai <me@roudai.net>
2022-11-01 15:32:35 +0900

    ビルド高速化

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,11 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    parallel: true,
+    cache: true,
+    hardSource: true,
+  },
 
   router: {
     base: '/komachan/',
