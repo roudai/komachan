78a1847a6c6d78d4e40c5abe6e1e4f43350840f6
roudai <me@roudai.net>
2022-11-05 23:57:15 +0900

    タイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'komachan',
+    title: '#1にち1コマちゃん',
     htmlAttrs: {
       lang: 'ja',
     },
