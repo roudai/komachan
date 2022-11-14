8fd5edac603f3693c2f61299d003b538026610ae
roudai <me@roudai.net>
2022-11-15 08:18:45 +0900

    不要なscript削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,28 +39,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
-        },
-      ],
-    };
-  },
-};
-</script>
-
 <style>
 body {
   font-family: 'Mochiy Pop P One', sans-serif;
