86df178da462e26412b14fc7373409c9870b250c
roudai <me@roudai.net>
2022-11-24 19:32:49 +0900

    フッターコンポーネント化

---
+++ components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Presented by
+        <a class="twitter" href="https://twitter.com/w_ty_OvO">@w_ty_OvO</a>.
+        <a href="https://github.com/roudai/komachan" aria-label="githubを開く">
+          <img :src="require(`@/assets/GitHub-Mark-32px.webp`)" class="github" alt="github" />
+        </a>
+      </p>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="has-background-info-light">
     <section class="main-content columns ml-0 mr-0">
-      <div
-        class="container column is-12-mobile is-10-tablet is-6-desktop pl-0 pr-0"
-      >
+      <div class="container column is-12-mobile is-10-tablet is-6-desktop pl-0 pr-0">
         <Nuxt />
       </div>
     </section>
-
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          Presented by
-          <a class="twitter" href="https://twitter.com/w_ty_OvO">@w_ty_OvO</a>.
-          <a
-            href="https://github.com/roudai/komachan"
-            aria-label="githubを開く"
-          >
-            <img
-              :src="require(`@/assets/GitHub-Mark-32px.webp`)"
-              class="github"
-              alt="github"
-            />
-          </a>
-        </p>
-      </div>
-    </footer>
+    <footer-component></footer-component>
   </div>
 </template>
 
