d2642c3458bddeea90c6aa15c1ca0c0c699e2410
roudai <me@roudai.net>
2022-12-26 23:12:43 +0900

    favicon設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,8 @@ export default {
       lang: 'ja',
     },
     meta: [
+      { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#eef6fc' },
+      { hid: 'theme-color', name: 'theme-color', content: '#eef6fc' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
@@ -19,9 +21,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'shortcut icon', href: '/favicon.ico' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { hid: 'apple-touch-icon', rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { hid: 'icon32', rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { hid: 'icon16', rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { hid: 'manifest', rel: 'manifest', href: '/site.webmanifest' },
+      { hid: 'mask-icon', rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
     ],
   },
 
